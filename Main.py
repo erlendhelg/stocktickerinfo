87b23fbaa91f6b5461274721ad8c7bd0fab8e919
erlendhelg <erlendhelg@gmail.com>
2022-08-05 15:46:31 +0200

    =D

--- Main.py
+++ Main.py
@@ -1,8 +1,15 @@
 import requests
 import json
+import finnhub
+finnhub_client = finnhub.Client(api_key="cbmhtaqad3i035onbt50")
 
+# https://github.com/Finnhub-Stock-API/finnhub-python
+# https://finnhub.io/docs/api/
 
 url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 bitcoinprice = requests.get(url)
 
 print("Price of Bitcoin is: " + "\n" + "€" + bitcoinprice.json()["bpi"]["EUR"]["rate"])
+
+# Stock candles
+print(finnhub_client.company_basic_financials('AAPL', 'all'))
